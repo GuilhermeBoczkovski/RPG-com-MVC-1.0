222029fc2dd000d88e54b7c227881da8138b2ae6
GuilhermeBoczkovski <40305138+GuilhermeBoczkovski@users.noreply.github.com>
2018-10-15 11:23:49 -0300

    Add files via upload
    modo teste adicionado

--- ControladorGeral.java
+++ ControladorGeral.java
@@ -9,16 +9,16 @@ public class ControladorGeral {
         this.telaInicio = new TelaInicio(this);
     }
     
+    public ControladorGeral(int i){
+        this.telaInicio = new TelaInicio(this);
+    }
+    
     public void inicio(){
-        try{
-            telaInicio.mostraTelaInicio();
-        }catch(Exception e){
-            System.out.println(e);
-        }
+        telaInicio.mostraTelaInicio();
     }
 
     void play(String nome){
-        this.controladorPrincipal = new ControladorPrincipal(nome);
+        this.controladorPrincipal = new ControladorPrincipal(nome, 1);
         this.controladorPrincipal.escolheEncontro();
     }
     
